65e64e48bfd87c3fcbb30af1a9c335bf6f9572fe
Florian Kappes <florian.kappes@swarco.de>
2020-10-23 12:51:10 +0200

    Big Bang

---
+++ README.md
@@ -0,0 +1,22 @@
+Countdown
+---
+Simple react countdown application
+
+
+Usage
+---
+
+```
+npm start
+```
+
+Pass any ISO formatted date via
+
+```
+http://localhost:3000/?date=2020-12-01T:00:00.000Z
+```
+
+Attribution
+---
+
+Loosely based on https://github.com/ahfarmer/calculator
---
+++ src/component/Digit.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./Digit.css";
+
+export default class Digit extends React.Component {
+  static propTypes = {
+    value: PropTypes.number,
+    label: PropTypes.string,
+  };
+
+  visible = true;
+  render() {
+    return (
+      <div className="digit">
+        <div className="number">{this.format()}</div>
+        <div className="label">{this.props.label}</div>
+      </div>
+    );
+  }
+
+  format() {
+      if (this.props.value < 10 && this.props.value >= 0) {
+          return "0" + this.props.value;
+      };
+      if (this.props.value < 0) {
+          return -1 * this.props;
+      };
+      return this.props.value;
+  }
+}

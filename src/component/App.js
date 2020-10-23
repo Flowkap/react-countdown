18a7e51d8da809a337b6c45b4d359d666ef98a58
Florian Kappes <kappesf@gmail.com>
2020-10-23 12:51:10 +0200

    Big Bang

---
+++ src/component/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Counter from "./Counter";
+import "./App.css";
+
+export default class App extends React.Component {
+
+  date = new Date();
+
+  render() {
+    this.readData();
+    return (
+      <div className="container">
+        <div className="content">
+          <h1>{this.text}</h1>
+          <img src="me.png" alt="Flowkap" />
+          <Counter value={this.date} />
+        </div>
+      </div>
+    );
+  }
+
+  readData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const date = urlParams.get('date');
+    if (date) {
+      this.date = new Date(date);
+    }
+  }
+}
---
+++ src/component/Counter.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Digit from "./Digit";
+import PropTypes from "prop-types";
+
+import "./Counter.css";
+
+export default class Counter extends React.Component {
+  static propTypes = {
+    value: PropTypes.instanceOf(Date),
+  };
+
+  render() {
+    const calculated = this.calculate();
+    return (
+      <div className="counter">
+        {/* <Digit label="years" value={calculated.years} /> */}
+        <Digit label="weeks" value={calculated.weeks} />
+        <Digit label="days" value={calculated.days} />
+        <Digit label="hours" value={calculated.hours} />
+        <Digit label="minutes" value={calculated.minutes} />
+        <Digit label="seconds" value={calculated.seconds} />
+      </div>
+    );
+  }
+
+  calculate() {
+    var now = new Date();
+    var duration = Math.floor((this.props.value.getTime() - now.getTime()) / 1000);
+
+    console.log(duration);
+
+    var seconds = parseInt((duration) % 60),
+      minutes = parseInt((duration / 60) % 60),
+      hours = parseInt((duration / 3600) % 24),
+      days = parseInt((duration / (3600 * 24)) % 7),
+      weeks = parseInt((duration / (3600 * 24 * 7)) % 56);
+
+    return {
+      weeks,
+      days,
+      hours,
+      minutes,
+      seconds,
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.forceUpdate(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+}
